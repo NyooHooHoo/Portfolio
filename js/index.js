4e19176ce34ee37b2a2c4ccdd8cf17b1c2789a53
Aidan Wang <aidan@MacBook-2744.local>
2023-04-28 19:02:59 -0400

    formatting for oop page

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
 function copy(id) {
     var copyText = document.getElementById(id).innerText;
     navigator.clipboard.writeText(copyText);
+    document.getElementById(id + "-msg").style.visibility = "visible";
+    setTimeout(function() {
+        $('#' + id + '-msg').fadeOut('fast');
+    }, 700);
+    $('#' + id + '-msg').fadeIn(0);
 }
+
